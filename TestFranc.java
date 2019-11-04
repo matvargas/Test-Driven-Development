5a5987c7efe14ba8384c72ed7c41372e7195611c
Matheus Vargas <m6.vargas@gmail.com>
2019-11-04 13:32:16 -0300

    Roteiro #2

--- TestFranc.java
+++ TestFranc.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
- public class TestDollar {
+ public class TestFranc {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
