6a04a61e088fda539e6ed4dd975283f0cfb254d2
Matheus Vargas <m6.vargas@gmail.com>
2019-11-04 13:30:33 -0300

    Roteiro #2

--- TestDollar.java
+++ TestDollar.java
@@ -5,7 +5,13 @@ import static org.junit.Assert.*;
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-    }
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+     }
+
+    @Test
+     public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+     }
 }  
